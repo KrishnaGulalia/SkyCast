5c8a5773a041a93d237a1511db46fd1ca797f6ae
Tahseen Sarwar <tahseensarwar0017@gmail.com>
2026-05-06 22:45:34 +0530

    footer added and css updated

--- skycast/src/components/Footer.jsx
+++ skycast/src/components/Footer.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Footer.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer-inner'>
+        <div className='footer-brand'>
+          <span className='footer-logo'>⛅ SkyCast</span>
+          <p>Real-time weather, beautifully simple.</p>
+        </div>
+        <div className='footer-links'>
+          <p className='footer-links-title'>Pages</p>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </div>
+        <div className='footer-links'>
+          <p className='footer-links-title'>Data</p>
+          <a href='https://openweathermap.org' target='_blank' rel='noreferrer'>
+            OpenWeatherMap
+          </a>
+          <a href='https://openweathermap.org/api' target='_blank' rel='noreferrer'>
+            Weather API
+          </a>
+        </div>
+      </div>
+      <div className='footer-bottom'>
+        <p>© 2026 SkyCast · Built with React & Vite</p>
+      </div>
+    </div>
   )
 }
