4d542003937f3e55a32f356fd4066fdca8a4a386
Tahseen Sarwar <tahseensarwar0017@gmail.com>
2026-05-06 22:12:56 +0530

    added routing links

--- skycast/src/App.jsx
+++ skycast/src/App.jsx
@@ -4,14 +4,22 @@ import Weather from './components/Weather'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import About from './components/About'
-import Api_Docs from './components/Api_Docs'
+import Api_Docs from './components/Api_Doc'
 import './index.css'
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className='app'>
-        <Weather />
+        <Header />
+        <div className='main-content'>
+          <Routes>
+            <Route path='/' element={<Weather />}/>
+            <Route path='/About' element={<About />}/>
+            <Route path='/Api_Doc' element={<Api_Docs />}/>
+          </Routes>
+
+        </div>
       </div>
     </BrowserRouter>
   )
--- skycast/src/components/Header.jsx
+++ skycast/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <Link to='/'>Home</Link>
+      <Link to='/About'>About</Link>
+      <Link to='/Api_Doc'>API Documentation</Link>
+    </div>
   )
 }
