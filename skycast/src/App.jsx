eb5357a1425252338b8a2c87ef6b5e9e6c09cb81
Tahseen Sarwar <tahseensarwar0017@gmail.com>
2026-05-06 22:01:54 +0530

    imported components

--- skycast/src/App.jsx
+++ skycast/src/App.jsx
@@ -1,6 +1,11 @@
 import React, { use } from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Weather from './components/Weather'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About'
+import Api_Docs from './components/Api_Docs'
+import './index.css'
 
 export default function App() {
   return (
